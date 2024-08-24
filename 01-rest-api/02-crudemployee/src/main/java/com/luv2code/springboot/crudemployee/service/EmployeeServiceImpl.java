ba7ead1a56e4ad99f7d8351af3bf476d97c4484d
andersonlscesar <andersonlscesar@gmail.com>
2024-08-24 15:22:55 -0300

    Possibilidade de encontrar um funcionário especifico adicionada

--- 01-rest-api/02-crudemployee/src/main/java/com/luv2code/springboot/crudemployee/service/EmployeeServiceImpl.java
+++ 01-rest-api/02-crudemployee/src/main/java/com/luv2code/springboot/crudemployee/service/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.luv2code.springboot.crudemployee.service;
 
 import com.luv2code.springboot.crudemployee.dao.EmployeeDAO;
 import com.luv2code.springboot.crudemployee.entity.Employee;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +21,22 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return employeeDAO.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Employee save(Employee employee) {
+        return employeeDAO.save(employee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int id) {
+        employeeDAO.deleteById(id);
     }
 }
